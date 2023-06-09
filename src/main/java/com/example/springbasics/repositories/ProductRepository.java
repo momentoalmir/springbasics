e70e7c85b494b7f648fa4a5b29b8444e81e4aec4
momentoalmir <momentoalmir@gmail.com>
2023-06-09 08:42:30 -0300

    Add Spring Dev Tools e remove unnecessary repo annotation

--- src/main/java/com/example/springbasics/repositories/ProductRepository.java
+++ src/main/java/com/example/springbasics/repositories/ProductRepository.java
@@ -2,10 +2,10 @@ package com.example.springbasics.repositories;
 
 import com.example.springbasics.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import org.springframework.stereotype.Repository; // Not needed because JpaRepository already has it
 
 import java.util.UUID;
 
-@Repository
+// @Repository - Not needed because JpaRepository already has it
 public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
 }
